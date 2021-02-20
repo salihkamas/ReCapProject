78eaa90cdac09993f2a11fa90b0df1ee19c3752c
Salih Kamaş <salihkamasy@gmail.com>
2021-02-20 17:30:57 +0300

    20.02

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results.Abstract;
 using Core.Utilities.Results.Concrete;
 using DataAccess.Abstract;
@@ -20,16 +22,9 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            if (car.CarName.Length<2)
-            {
-                return new ErrorResult(Messages.CarNameInvalid);
-            }
-            if (car.DailyPrice<=0)
-            {
-                return new ErrorResult(Messages.CarDailyPriceInvalid);
-            }
             _carDal.Add(car);
             return new SuccessResult(Messages.SuccesAdded);
         }
---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.CarName).NotEmpty().WithMessage("Car name cannot be empty");
+            RuleFor(c => c.CarName).MinimumLength(2).WithMessage("Car name must be at least two characters long");
+            RuleFor(c => c.DailyPrice).NotEmpty().WithMessage("Daily Price cannot be empty");
+            RuleFor(c => c.DailyPrice).GreaterThan(0).WithMessage("Daily price must be greater than zero");
+        }
+    }
+}
